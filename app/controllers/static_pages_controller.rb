21c0e18389e7718348c4e0e84de321f3f031bdc3
Ravin <ravin@gocardless.com>
2014-10-22 19:24:45 +0100

    create static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+def welcome
+end
+
+
+
+end
